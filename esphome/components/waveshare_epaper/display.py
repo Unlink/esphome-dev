71404ed2db99ac96e16ed54a0d18eca164883874
Michal Ďuračík <michal@duracik.sk>
2024-10-25 15:36:17 +0200

    Import

--- esphome/components/waveshare_epaper/display.py
+++ esphome/components/waveshare_epaper/display.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
 from esphome import core, pins
 from esphome.components import display, spi
 from esphome.const import (
